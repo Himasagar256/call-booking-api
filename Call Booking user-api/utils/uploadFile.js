b11408a83141d821054058f0a9754fb4b9b5887e
Himasagar256 <128246280+Himasagar256@users.noreply.github.com>
2023-09-13 10:08:01 +0530

    Update uploadFile.js

--- Call Booking user-api/utils/uploadFile.js
+++ Call Booking user-api/utils/uploadFile.js
@@ -1,7 +1,7 @@
 const multer  = require('multer');
 const {GridFsStorage} = require('multer-gridfs-storage');
 const storage = new GridFsStorage({
-    url: "mongodb://localhost:27017",
+    url: "mongodb://localhost:27017/mydb",
     file: (req,file) => {
         if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')  {
             let filename = 'file_'+Date.now()

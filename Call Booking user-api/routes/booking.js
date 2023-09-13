632b3f97cc015ffd00a5d944fab60edbe93616eb
Himasagar256 <128246280+Himasagar256@users.noreply.github.com>
2023-09-13 09:18:25 +0530

    Add files via upload

---
+++ Call Booking user-api/routes/booking.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { createBooking } = require('../controllers/bookingControllers');
+const { upload } = require('../utils/uploadFile');
+const router = express.Router();
+router.post('/', upload.single('image'), createBooking);
+module.exports = router;
